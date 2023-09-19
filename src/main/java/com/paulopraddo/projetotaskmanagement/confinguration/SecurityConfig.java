a832f9d6ea408de02e53794fda394359e19b2a27
unknown <paulopraddo011@gmail.com>
2023-09-19 14:02:34 -0300

    autorizando requisições

--- src/main/java/com/paulopraddo/projetotaskmanagement/confinguration/SecurityConfig.java
+++ src/main/java/com/paulopraddo/projetotaskmanagement/confinguration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.paulopraddo.projetotaskmanagement.confinguration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,6 +17,9 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/salvarTarefa").hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .build();
     }
 
